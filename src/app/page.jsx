91da62e626d8d1d51bd0a4edd570bd230f0f890b
mohammadjalizi <mjalizi1376@gmail.com>
2024-11-08 06:43:58 -0500

    Add navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-
+import { ShoppingCart,User } from 'lucide-react';
 export default function Home() {
   return (
-  <div className=" text-center   my-30 ">
-  
-  mohammmad
-  
+  <div className="  container flex justify-between  navbar   my-3 ">
+    
+<div className=" flex gap-4 items-center">
+
+<div>
+<ShoppingCart />
+  </div>
+<div>
+<User />
+</div>
+</div>
+  <div>
+ logo coffie
+  </div>
   </div>
   );
 }
